dc0ed629636777a0d9ed1a97b3e2ac1bcdd410a0
Piyush kumar <singhpiyushrajput8279l@gmail.com>
2026-06-19 17:50:34 +0530

    remove

--- server/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { SecureUserInfo } from "../controllers/auth.controller";
-
-declare global {
-    namespace Express {
-      interface Request {
-        user?: SecureUserInfo; 
-      }
-    }
-}
